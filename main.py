10d331152e4dc7defbb79c9f5b9342c9f2e3709e
Anderson Diniz <andersoncd123@gmail.com>
2026-08-29 19:57:17 -0300

    Ajustando configurações de segurança

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 import asyncio
 import requests
 import difflib
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 
@@ -53,6 +54,18 @@ async def lifespan(app: FastAPI):
     task.cancel()
 
 app = FastAPI(lifespan=lifespan)
+
+NOME_CABECALHO_API = "X-Pyxis-Key"
+CHAVE_SECRETA = os.environ.get("PYXIS_API_KEY")
+api_key_header = APIKeyHeader(name=NOME_CABECALHO_API, auto_error=False)
+
+async def verificar_api_key(api_key: str = Security(api_key_header)):
+    if api_key != CHAVE_SECRETA:
+        raise HTTPException(
+            status_code=403, 
+            detail="Acesso Negado: Você não é o Grão-Duque."
+        )
+    return api_key
 
 class RequisicaoMinecraft(BaseModel):
     comando: str
@@ -155,7 +168,7 @@ def chamar_gemini_rag(termo_pesquisa: str, fonte: str, tentativas=3):
         return f"Houve uma falha na matriz de dados, Vossa Graça: {dados}"
 
 @app.post("/agente")
-def processar_comando(req: RequisicaoMinecraft):
+def processar_comando(req: RequisicaoMinecraft, api_key: str = Depends(verificar_api_key)):
     resultado_roteador = chamar_roteador_gemini(req.comando)
     
     if resultado_roteador.get("acao") == "consultarBase":
